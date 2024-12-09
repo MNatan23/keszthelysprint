720d312ad934650045dcd97952a7f18243bc3ab6
MNatan23 <163414251+MNatan23@users.noreply.github.com>
2024-12-09 13:56:13 +0100

    Add project files.

---
+++ keszthelysprint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace keszthelysprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<versenyzo> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new versenyzo(sor));
+            }
+            Console.WriteLine($"A versenyt {versenyzok.Count} versenyzők fejezték be!");
+        }
+        int elit_junior = 0;
+             
+       foreach (var v in versenyzők)
+	{
+     if (v.Kategória == "elit junior")
+	{
+     elit_junior++;
+	}
+    
+	} 
+   
+}
---
+++ keszthelysprint/versenyzo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keszthelysprint
+{
+    internal class versenyzo
+    {
+        public string Nev { get; set; }
+        public int Szuletesieve { get; set; }
+        public int  Rajtszam { get; set; }
+
+        public string Neme { get; set; }
+        public string Életkor { get; set; }
+        public string Elsoeredmeny { get; set; }
+        public string Masodikeredmeny { get; set; }
+        public string Harmadikeredmeny { get; set; }
+        public string Negyedikeredmeny { get; set; }
+        public string Otodikeredmeny { get; set; }
+
+        public versenyzo(string sor)
+        {
+            string[] darabok = sor.Split(';');
+
+            Nev = darabok[0];
+            Szuletesieve = int.Parse(darabok[1]);
+            Rajtszam = int.Parse( darabok[2]);
+            Neme = darabok[3];
+            Életkor = darabok[4];
+            Elsoeredmeny = darabok[5];
+            Masodikeredmeny = darabok[6];
+            Harmadikeredmeny = darabok[7];
+            Negyedikeredmeny = darabok[8];
+            Otodikeredmeny = darabok[9];
+        }
+
+
+
+
+
+    }
+}
